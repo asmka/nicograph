fa3556e1676da3d64c8d5761c4d60a246c0b87b1
dff9th <dff9th@gmail.com>
2020-02-01 10:36:39 +0000

    Introduce gulp

---
+++ webpack.common.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: {
+        main: './src/js/main.js',
+        reqAdJson: './src/js/reqAdJson.js',
+        reqCmtJson: './src/js/reqCmtJson.js',
+        reqVideoDoc: './src/js/reqVideoDoc.js'
+    },
+    output: {
+        filename: '[name].js'
+    }
+};
