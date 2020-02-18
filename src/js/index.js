ce5afe1b5b74d65013370ecd66d75bfb51290ea8
dff9th <dff9th@gmail.com>
2020-02-18 05:30:27 +0000

    Remove temporary process for another extension

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,12 @@ function getWatchAppJsURI() {
     const scriptTags = Array.from(document.getElementsByTagName('script'));
     const watchAppJsRegExp = /watch_app_.*\.js/;
 
+    // Attension:
+    // There are two URIs which are "watch_app_*.js" and "watch_app_*?by-<extention name>.js"
+    // if another extension is introduced which overwrites watchApp with the same method.
+    // Then, we get "watch_app_*?by-<extension name>.js" for enabling all extensions which
+    // include this one,
+    // As a result, we inject "watch_app_*?by-<extension name>&by-nicograph.js".
     const target = scriptTags.filter((script) => {
         return watchAppJsRegExp.test(script.src);
     }).pop();
