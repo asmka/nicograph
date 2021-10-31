f26bae9483d500153212551401d93558cb94a54b
dff9th <33749863+dff9th@users.noreply.github.com>
2021-10-31 20:03:05 +0900

    Hotfix/#34 there are no graph cases (#35)
    * tmp
    
    * Read scripts in onder with async/await (Close #34)

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,28 @@ Released under the MIT license
 https://github.com/noradium/dac/blob/master/src/scripts/index.js
 */
 
-inject(chrome.extension.getURL("js/hack_lib.js"));
-const watchAppJsURI = getWatchAppJsURI();
-inject(
-  `${watchAppJsURI}${watchAppJsURI.indexOf("?") === -1 ? "?" : "&"}by-nicograph`
-);
+(async () => {
+  await inject(chrome.extension.getURL("js/hack_lib.js"));
+  // Get watch app URI just before inject in case of the late loading by another extensions
+  const watchAppJsURI = getWatchAppJsURI();
+  await inject(
+    `${watchAppJsURI}${
+      watchAppJsURI.indexOf("?") === -1 ? "?" : "&"
+    }by-nicograph`
+  );
+})();
 
-function inject(src) {
-  const s = document.createElement("script");
-  s.setAttribute("type", "text/javascript");
-  s.setAttribute("src", src);
+async function inject(src) {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement("script");
+    s.setAttribute("type", "text/javascript");
+    s.setAttribute("src", src);
+    s.onload = () => {
+      resolve();
+    };
 
-  document.body.appendChild(s);
+    document.body.appendChild(s);
+  });
 }
 
 function getWatchAppJsURI() {
