79027deeaef17f0607616e1166df3e83a344d476
dff9th <dff9th@gmail.com>
2021-11-02 00:22:38 +0900

    [Tmp] Saccess to load

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mode: "production",
   entry: {
-    index: "./src/index.js",
-    hack_lib: ["./src/hack_fetch_thread.js", "./src/hack_get_nicoads.js"],
-    background: "./src/background.js",
+    index: "./src/js/index.js",
+    hack_lib: ["./src/js/hack_fetch_thread.js", "./src/js/hack_get_nicoads.js"],
+    background: "./src/js/background.js",
   },
   output: {
     filename: "[name].js",
