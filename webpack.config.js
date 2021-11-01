d69cc273cd691c10a7a58a7f7ee0fc7e8b379a15
dff9th <dff9th@gmail.com>
2021-11-02 00:16:45 +0900

    tmp

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,11 @@
-const path = require('path');
-
 module.exports = {
+  mode: "production",
   entry: {
     index: "./src/index.js",
-    //hack_fetch_thread: './src/hack_fetch_thread.js',
-    //hack_get_nicoads: './src/hack_get_nicoads.js',
     hack_lib: ["./src/hack_fetch_thread.js", "./src/hack_get_nicoads.js"],
     background: "./src/background.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, 'release'),
   },
-  /*
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                cmt_graph: {
-                    name: 'cmt_graph',
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
-        },
-    }
-    */
 };
