90ba1223e8067ef7bb6174c1f9e4afd36881f13e
asmka <asmka9@gmail.com>
2022-06-22 23:06:01 +0900

    tmp

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ type ForkType = "owner" | "main" | "easy";
 interface GlobalComment {
   id: number;
 }
+
 export interface FetchCommentsRequest {
   params: {
     targets: Array<{
@@ -24,4 +25,37 @@ export interface FetchCommentsResponse {
       globalComments: GlobalComment[];
     };
   };
+}
+
+export function fetchComments() {
+  const apiData = parseApiData();
+  const nvComment = apiData.comment.nvComment;
+
+  const uri = "https://nvcomment.nicovideo.jp/v1/threads";
+  const body: FetchCommentsRequest = {
+    params: nvComment.params,
+    threadKey: nvComment.threadKey,
+    additionals: {},
+  };
+  const options = {
+    method: "POST",
+    headers: new Headers({
+      "X-Frontend-Id": "6",
+      "X-Frontend-Version": "0",
+    }),
+    body: JSON.stringify(body),
+  };
+
+  return fetch(uri, options);
+}
+
+function parseApiData() {
+  const apiDataValue = document
+    .getElementById("js-initial-watch-data")
+    ?.getAttribute("data-api-data");
+  if (apiDataValue === undefined || apiDataValue === null) {
+    throw new Error('"data-api-data" is not exist.');
+  }
+
+  return JSON.parse(apiDataValue);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,9 @@
-import { FetchCommentsRequest } from "./api";
+import { fetchComments } from "./api";
 
 main();
 
 function main() {
-  const apiData = parseApiData();
-  console.debug("apiData: ", apiData);
-
-  const nvComment = apiData.comment.nvComment;
-  console.debug("nvComment", nvComment);
-
-  const body: FetchCommentsRequest = {
-    params: {
-      targets: nvComment.params.targets,
-      language: nvComment.params.language,
-    },
-    threadKey: nvComment.threadKey,
-    additionals: {},
-  };
-
-  // const fmt: FetchCommentsRequest = nvComment;
-  // console.debug("fmt", fmt);
-
-  // fetchComments(apiData);
-  nvComment.additionals = {};
-  const uri = "https://nvcomment.nicovideo.jp/v1/threads";
-  const options = {
-    method: "POST",
-    headers: new Headers({
-      "X-Frontend-Id": "6",
-      "X-Frontend-Version": "0",
-      "X-Client-Os-Type": "others",
-    }),
-    // body: JSON.stringify(nvComment),
-    body: JSON.stringify(body),
-  };
-  fetch(uri, options)
+  fetchComments()
     .then((response) => {
       console.debug(response);
       return response.json();
@@ -43,37 +12,3 @@ function main() {
       console.debug(data);
     });
 }
-
-function parseApiData() {
-  const apiDataValue = document
-    .getElementById("js-initial-watch-data")
-    ?.getAttribute("data-api-data");
-  if (apiDataValue === undefined || apiDataValue === null) {
-    throw new Error('"data-api-data" is not exist.');
-  }
-
-  return JSON.parse(apiDataValue);
-}
-
-// function fetchComments(apiData: any) {
-//   const nvComment = apiData.comment.nvComment;
-//   console.debug("nvComment", nvComment);
-//   const uri = "https://nvcomment.nicovideo.jp/v1/threads";
-//   fetch(uri, options)
-//     .then((response) => {})
-//     .then((data) => {});
-//   return fetch();
-//
-//   // fetch("https://nvcomment.nicovideo.jp/v1/threads", {
-//   //   method: "POST",
-//   //   mode: "cors",
-//   //   cache: "no-cache",
-//   //   credentials: "same-origin",
-//   //   headers: [("Content-Type": "application/json")],
-//   //   redirect: "follow",
-//   //   reffferPolicy: "no-referrer",
-//   //   body: JSON.stringify(data),
-//   // })
-//   //   .then((response) => console.log(response.json()))
-//   //   .then((data) => console.log(data));
-// }
