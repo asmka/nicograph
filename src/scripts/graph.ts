49fef64f25e83a05bf241a6a48ef9ebc06e59cbd
asmka <asmka9@gmail.com>
2022-08-13 17:05:45 +0900

    Follow to resized seekbar

--- src/scripts/graph.ts
+++ src/scripts/graph.ts
@@ -1,88 +1,101 @@
 import { Thread } from "./api/comment";
 
-export function drawGraph(threads: Thread[], isNicoad: boolean) {
-  const drawTgt = document.getElementsByClassName("XSlider")[0];
-  const cmtCnts = getCommentCounts(threads, isNicoad);
+const DIVISION = 100;
+const NICOAD_TIME_MS = 10 * 1000;
+const GRAPH_ELEMENT_ID = "nicograph_element";
 
-  // Sum all comments
-  let maxCnt = 1;
-  for (let i = 0; i < cmtCnts.length; i++) {
-    if (cmtCnts[i] > maxCnt) {
-      maxCnt = cmtCnts[i];
+export class CommentGraph {
+  private readonly target: HTMLElement;
+  readonly elem: HTMLElement;
+
+  constructor(threads: Thread[], isNicoad: boolean, target: HTMLElement) {
+    const counts = this.getCommentCounts(threads, isNicoad);
+
+    // Set graph area
+    const graphWidth = target.getBoundingClientRect().width;
+    const graphHeight = 18;
+    const graphFixTop = 4;
+
+    // Insert a graph element
+    const elem = document.createElement("div");
+    elem.id = GRAPH_ELEMENT_ID;
+    elem.style.position = "relative";
+    elem.style.top = graphFixTop + "px";
+    elem.style.lineHeight = graphHeight + "px";
+
+    // Create comment rectangles
+    const barWidth = graphWidth / counts.length;
+    const maxBarHeight = graphHeight;
+    const maxCount = Math.max.apply(null, counts);
+    for (let i = 0; i < counts.length; i++) {
+      // Create a rectangle element
+      const e = document.createElement("div");
+      e.style.backgroundColor = "#66CCFF";
+      e.style.opacity = "0.5";
+      e.style.display = "inline-block";
+      e.style.verticalAlign = "bottom";
+
+      // Calc rectangle
+      const eWidth = barWidth;
+      const eHeight = Math.floor(maxBarHeight * (counts[i] / maxCount));
+      e.style.width = eWidth + "px";
+      e.style.height = eHeight + "px";
+
+      // Add element
+      elem.appendChild(e);
+    }
+
+    this.setResizeObserver(target);
+
+    this.target = target;
+    this.elem = elem;
+  }
+
+  private setResizeObserver(target: HTMLElement): ResizeObserver {
+    const observer = new ResizeObserver(() => {
+      this.redraw();
+    });
+    observer.observe(target);
+
+    return observer;
+  }
+
+  private redraw(): void {
+    const graphWidth = this.target.getBoundingClientRect().width;
+    const rects = this.elem.children as HTMLCollectionOf<HTMLElement>;
+    const barWidth = graphWidth / rects.length;
+    for (const e of rects) {
+      e.style.width = barWidth + "px";
     }
   }
 
-  // Insert a graph element
-  const graphId = "CmtGraph";
-  let graphElem = document.getElementById(graphId);
-  // Remove before graph
-  if (graphElem) {
-    graphElem.remove();
-  }
-  graphElem = document.createElement("div");
-  graphElem.id = graphId;
+  private getCommentCounts(threads: Thread[], isNicoad: boolean): number[] {
+    const movieTimeMs = this.getMovieTimeMs();
+    const totalTimeMs = movieTimeMs + (isNicoad ? NICOAD_TIME_MS : 0);
 
-  // Set graph area
-  const graphWidth = drawTgt.getBoundingClientRect().width;
-  const graphHeight = 18;
-  const graphFixTop = 4;
-  graphElem.style.position = "relative";
-  graphElem.style.top = graphFixTop + "px";
-  graphElem.style.lineHeight = graphHeight + "px";
-  drawTgt.insertBefore(graphElem, drawTgt.firstChild);
-
-  // Create comment rectangles
-  const barWidth = graphWidth / cmtCnts.length;
-  const maxBarHeight = graphHeight;
-  for (let i = 0; i < cmtCnts.length; i++) {
-    // Create a rectangle element
-    const e = document.createElement("div");
-    e.className = "CmtRect";
-    e.style.backgroundColor = "#66CCFF";
-    e.style.opacity = "0.5";
-    e.style.display = "inline-block";
-    e.style.verticalAlign = "bottom";
-
-    // Calc rectangle
-    const eWidth = barWidth;
-    const eHeight = Math.floor(maxBarHeight * (cmtCnts[i] / maxCnt));
-    e.style.width = eWidth + "px";
-    e.style.height = eHeight + "px";
-
-    // Add element
-    graphElem.appendChild(e);
-  }
-}
-
-function getCommentCounts(threads: Thread[], isNicoad: boolean): number[] {
-  const DIVISION = 100;
-  const NICOAD_TIME_MS = 10 * 1000;
-  const movieTimeMs = getMovieTimeMs();
-
-  const totalTimeMs = movieTimeMs + (isNicoad ? NICOAD_TIME_MS : 0);
-
-  const commentCounts = new Array<number>(DIVISION);
-  commentCounts.fill(0);
-  for (const t of threads) {
-    for (const c of t.comments) {
-      const p = Math.min(
-        Math.floor((c.vposMs * DIVISION) / totalTimeMs),
-        DIVISION - 1
-      );
-      commentCounts[p]++;
+    const commentCounts = new Array<number>(DIVISION);
+    commentCounts.fill(0);
+    for (const t of threads) {
+      for (const c of t.comments) {
+        const p = Math.min(
+          Math.floor((c.vposMs * DIVISION) / totalTimeMs),
+          DIVISION - 1
+        );
+        commentCounts[p]++;
+      }
     }
+
+    return commentCounts;
   }
 
-  return commentCounts;
-}
+  private getMovieTimeMs(): number {
+    const movieDuration = document.getElementsByClassName(
+      "PlayTimeFormatter PlayerPlayTime-duration"
+    )[0].innerHTML;
 
-function getMovieTimeMs(): number {
-  const movieDuration = document.getElementsByClassName(
-    "PlayTimeFormatter PlayerPlayTime-duration"
-  )[0].innerHTML;
+    const durs = movieDuration.split(":");
+    const movieTimeMs = (parseInt(durs[0]) * 60 + parseInt(durs[1])) * 1000;
 
-  const durs = movieDuration.split(":");
-  const movieTimeMs = (parseInt(durs[0]) * 60 + parseInt(durs[1])) * 1000;
-
-  return movieTimeMs;
+    return movieTimeMs;
+  }
 }
