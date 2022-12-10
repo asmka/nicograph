94731edb615fd70d7480cad08d224a9d0ebb5edf
asmka <asmka9@gmail.com>
2022-12-11 00:26:31 +0900

    feat: enable on unactivated tab

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,33 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (
-    tab.url?.startsWith("https://www.nicovideo.jp/watch") &&
-    changeInfo.status === "complete"
-  ) {
-    chrome.tabs.sendMessage(tabId, {
-      message: "video_loaded",
+const isNicovideoTab = (tab: chrome.tabs.Tab): boolean => {
+  return !!tab.url?.startsWith("https://www.nicovideo.jp/watch");
+};
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!isNicovideoTab(tab)) return;
+  if (changeInfo.status !== "complete") return;
+
+  try {
+    await chrome.tabs.sendMessage(tabId, {
+      message: "updateCompleted",
       url: tab.url,
     });
+  } catch (error) {
+    console.debug("sendMessage is failed");
+    console.debug(error);
+  }
+});
+
+chrome.tabs.onActivated.addListener(async (activateInfo) => {
+  const tab = await chrome.tabs.get(activateInfo.tabId);
+  if (!isNicovideoTab(tab)) return;
+
+  try {
+    await chrome.tabs.sendMessage(activateInfo.tabId, {
+      message: "activated",
+      url: tab.url,
+    });
+  } catch (error) {
+    console.debug("sendMessage is failed");
+    console.debug(error);
   }
 });
